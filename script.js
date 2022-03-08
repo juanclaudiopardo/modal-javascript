65552f54f5538c775122db859b38cc6e31639edc
Juan Pardo <josefrecinosanitarios@yahoo.com.ar>
2022-03-08 17:16:03 -0300

    termine el proyecto

--- script.js
+++ script.js
@@ -19,6 +19,12 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
 // btnCloseModal.addEventListener('click', function () {
 //   modal.classList.add('hidden');
 //   overlay.classList.add('hidden');
